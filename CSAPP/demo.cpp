942935e2b1f35d3a93716fe9d999652c44f9f74f
Guo binyun <80152856+bingo-gby@users.noreply.github.com>
2024-04-19 21:43:11 +0800

    update day2

--- CSAPP/demo.cpp
+++ CSAPP/demo.cpp
@@ -2,6 +2,8 @@
 #include<bitset>
 using namespace std;
 
+
+namespace J1{
 int main(){
     int number = -4;
     int number1 = 7;
@@ -9,5 +11,27 @@ int main(){
     std::cout << binaryRepresentation << std::endl; // 1110
     std::bitset<8> binaryRepresentation1(number);
     std::cout << binaryRepresentation1 << std::endl; // 0111
+    return 0;
+}
+}
+// 打印数字顺序
+namespace J2{
+    typedef unsigned char * byte_pointer;  // 用指针数组
+    void show_bytes(byte_pointer start, size_t len){
+        size_t i;
+        for(i = 0; i < len; i++){
+            printf("%.2x\n", start[i]);
+        }
+        printf("\n");
+    }
+    int main(){
+        int number = 15213;
+        show_bytes((byte_pointer) &number, sizeof(int));
+        return 0;
+    }
+}
+int main(){
+    // J1::main();
+    J2::main();
     return 0;
 }
